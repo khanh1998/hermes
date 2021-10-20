e1a4819b7ed80da4cb9bf643f4c52f5000321751
Bui Quoc Khanh <quockhanh.abc@gmail.com>
2021-10-20 21:10:34 +0700

    feat: simple socket server using gobwas/ws

---
+++ socket/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gobwas/ws"
+	"github.com/gobwas/ws/wsutil"
+)
+
+func main() {
+	http.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		conn, _, _, err := ws.UpgradeHTTP(r, w)
+		if err != nil {
+			log.Panic(err)
+		}
+		go func() {
+			defer conn.Close()
+			for {
+				msg, op, err := wsutil.ReadClientData(conn)
+				if err != nil {
+					log.Panic(err)
+				}
+				err = wsutil.WriteServerMessage(conn, op, msg)
+				if err != nil {
+					log.Panic(err)
+				}
+			}
+		}()
+	}))
+}
