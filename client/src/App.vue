e2462fdf98130634b27f0936393ed56638e139ce
Bui Quoc Khanh <quockhanh.abc@gmail.com>
2021-10-20 22:00:58 +0700

    feat: simple client using vue3 typescript

---
+++ client/src/App.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+  data: function () {
+    return {
+      ws: WebSocket,
+    };
+  },
+  created() {
+    const ws = new WebSocket("ws://localhost:8080")
+    ws.onmessage = (ev: MessageEvent<any>) => {
+      console.log({ message: ev})
+    }
+    const message = {
+      name: "khanh",
+      message: "i love you",
+      time: Date.now(),
+    }
+    ws.onopen = (ev: Event) => {
+      console.log({ open: ev })
+      ws.send(JSON.stringify(message))
+    }
+  },
+});
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- socket/main.go
+++ socket/main.go
@@ -9,7 +9,8 @@ import (
 )
 
 func main() {
-	http.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	err := http.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("new connection")
 		conn, _, _, err := ws.UpgradeHTTP(r, w)
 		if err != nil {
 			log.Panic(err)
@@ -19,13 +20,18 @@ func main() {
 			for {
 				msg, op, err := wsutil.ReadClientData(conn)
 				if err != nil {
-					log.Panic(err)
+					//log.Panic(err)
 				}
 				err = wsutil.WriteServerMessage(conn, op, msg)
 				if err != nil {
-					log.Panic(err)
+					//log.Panic(err)
 				}
 			}
 		}()
 	}))
+
+	if err != nil {
+		log.Panic(err)
+	}
+	log.Println("websocket server listening on port 8080")
 }
